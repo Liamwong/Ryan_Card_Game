f4fb014bec32a7cb58ccf62224a97d125100f16f
Ryan Sellinger <ryansellinger@MacBook-Air.verizon.net>
2021-08-04 23:59:43 -0400

    created drawCard function

--- src/com/personal/cardGame/Ladder.java
+++ src/com/personal/cardGame/Ladder.java
@@ -4,5 +4,12 @@ public class Ladder {
     public Deck deck = new Deck();
 
 
+    public Ladder(){
+        // pull out ace
+        // shuffle deck
+        // draw cards when necessary
+
+    }
+
 
 }
